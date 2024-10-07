4f9ef9621646d19dc4cba7c20a7099b31b3f1e28
ybespalava <ybespalava@icloud.com>
2024-10-07 22:55:16 +0200

    feat: add code block

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = 'files/fileToWrite.txt';
+  const filePath = join(__dirname, fileToWrite);
+
+  const writeStream = createWriteStream(filePath);
+
+  // Pipe the data from process.stdin to the writable stream
+  process.stdin.pipe(writeStream);
+
+  // Handle events for the Writable stream
+  writeStream.on('finish', () => {
+    console.log(`Write operation completed. Data written to ${fileToWrite}`);
+  });
+
+  writeStream.on('error', (error) => {
+    console.error(`FS operation failed: ${error.message}`);
+  });
 };
 
 await write();
