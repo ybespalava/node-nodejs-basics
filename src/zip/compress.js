557dee044ff930e64601af42a69e8b961ebf102e
ybespalava <ybespalava@icloud.com>
2024-10-07 22:55:53 +0200

    feat: add code block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = 'files/fileToCompress.txt';
+  const filePath = join(__dirname, fileToCompress);
+  const archivePath = join(__dirname, 'archive.gz');
+
+  const readStream = createReadStream(filePath);
+
+  const gzipStream = createGzip();
+
+  const writeStream = createWriteStream(archivePath);
+
+  readStream.pipe(gzipStream).pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log(`Compression completed. File compressed to ${archivePath}`);
+  });
+
+  writeStream.on('error', (error) => {
+    console.error(`Compression failed: ${error.message}`);
+  });
 };
 
 await compress();
