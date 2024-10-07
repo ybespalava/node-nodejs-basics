c598115c3c8537ede172357ea9f8fd0d47af94c2
ybespalava <ybespalava@icloud.com>
2024-10-07 22:45:26 +0200

    feat: add code block

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+      const numCPUs = os.cpus().length;
+  const workers = [];
+  const results = [];
+
+  const workerPromises = Array.from({ length: numCPUs }, (_, index) => {
+    const workerData = index + 10;
+    return new Promise((resolve, reject) => {
+      const worker = new Worker('./worker.js', { workerData });
+
+      worker.on('message', (message) => {
+        results.push({ status: 'resolved', data: message });
+        resolve();
+      });
+
+      worker.on('error', (error) => {
+        results.push({ status: 'error', data: null });
+        reject(error);
+      });
+
+      workers.push(worker);
+    });
+  });
+
+  await Promise.all(workerPromises);
+
+  console.log(results);
+
+  workers.forEach(worker => worker.terminate());
 };
 
 await performCalculations();
