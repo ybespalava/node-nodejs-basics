433fc755cfcd9fc0c61a56bdbcbc0f3c0ab99961
ybespalava <ybespalava@icloud.com>
2024-10-07 22:32:56 +0200

    feat: add code block

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { promises as fsPromises } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const folderPath = 'files';
+  const filePath = join(folderPath, fileName);
+  const fileContent = 'I am fresh and young';
+
+  try {
+    await fsPromises.access(filePath);
+
+    throw new Error('FS operation failed: File already exists');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fsPromises.writeFile(filePath, fileContent);
+      console.log(`File ${fileName} created successfully at ${folderPath}`);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
