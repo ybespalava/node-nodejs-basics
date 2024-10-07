4335a408307f0476bfd85db023a69840d7c82b4d
ybespalava <ybespalava@icloud.com>
2024-10-07 22:12:54 +0200

    feat: add code block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileToCalculateHashFor = 'files/fileToCalculateHashFor.txt';
+  const hash = createHash('sha256');
+
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(fileToCalculateHashFor);
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+      const hashResult = hash.digest('hex');
+      console.log(`SHA256 hash for ${fileToCalculateHashFor}: ${hashResult}`);
+      resolve(hashResult);
+    });
+
+    stream.on('error', (error) => {
+      reject(error);
+    });
+  });
 };
 
 await calculateHash();
