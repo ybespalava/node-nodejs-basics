193061a226521ddf1e99dd552f5735737bab66af
ybespalava <ybespalava@icloud.com>
2024-10-07 22:38:49 +0200

    feat: add code block

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { promises as fsPromises, constants } from 'fs';
+import { join } from 'path';
+
 const list = async () => {
-    // Write your code here 
+      const folderPath = 'files';
+
+  try {
+    await fsPromises.access(folderPath, constants.R_OK);
+
+    const files = await fsPromises.readdir(folderPath);
+    console.log('List of files in the folder:');
+    files.forEach(file => console.log(file));
+  } catch (error) {
+    // If the error is due to the folder not existing, rethrow it
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed: Folder does not exist');
+    } else {
+      throw error;
+    }
+  }
 };
 
 await list();
