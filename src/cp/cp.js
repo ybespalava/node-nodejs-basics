7a27f2257554755ae45af4cc6fe141b951af49b2
ybespalava <ybespalava@icloud.com>
2024-10-07 22:03:31 +0200

    feat: add code block

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+  const childProcess = spawn('node', ['script.js', ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+  childProcess.stdout.on('data', (data) => {
+    console.log(`Received from child process: ${data.toString()}`);
+  });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  childProcess.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.stdin.end(); 
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
