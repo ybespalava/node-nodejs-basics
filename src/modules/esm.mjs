dd3f78db0c10f104088932171f4ea15435543b6b
ybespalava <ybespalava@icloud.com>
2024-10-08 00:23:00 +0200

    feat: add code block

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,33 +1,39 @@
 import path from 'path';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
-import { readFile } from 'fs/promises';
-import './files/c.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import './files/c.js';  
 
 const random = Math.random();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 let unknownObject;
 
-const readJsonFile = async (filePath) => {
-    try {
-        const content = await readFile(filePath, 'utf-8');
-        return JSON.parse(content);
-    } catch (error) {
-        console.error(`Error reading JSON file at ${filePath}:`, error);
-        throw error;
+try {
+   
+    if (random > 0.5) {
+        unknownObject = await import('./files/a.json', {
+            assert: { type: 'json' }
+        });
+    } else {
+        unknownObject = await import('./files/b.json', {
+            assert: { type: 'json' }
+        });
     }
-};
+    console.log('Imported JSON Object:', unknownObject);
+} catch (error) {
+    console.error('Error importing JSON:', error.message);
+}
 
-const jsonFilePath = random > 0.5 ? './files/a.json' : './files/b.json';
 
-unknownObject = await readJsonFile(jsonFilePath);
-
-console.log(`Release ${release()}`);
-console.log(`Version ${version()}`);
+console.log(`Release: ${release()}`);
+console.log(`Version: ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
 
-console.log(`Path to current file is ${import.meta.url}`);
-console.log(`Path to current directory is ${path.dirname(new URL(import.meta.url).pathname)}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
@@ -35,11 +41,9 @@ const myServer = createServerHttp((_, res) => {
 
 const PORT = 3000;
 
-console.log(unknownObject);
-
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+    console.log('To terminate it, use Ctrl+C');
 });
 
 export { unknownObject, myServer };
