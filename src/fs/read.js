db5f111cd5f14e3343a8e62a3d8bb1d84f66159e
ybespalava <ybespalava@icloud.com>
2024-10-07 22:39:47 +0200

    feat: add code block

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { promises as fsPromises, constants } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = 'files/fileToRead.txt';
+  const filePath = join(__dirname, fileToRead);
+
+  try {
+    await fsPromises.access(filePath, constants.R_OK);
+    const content = await fsPromises.readFile(filePath, 'utf-8');
+    console.log(`Content of ${fileToRead}:`);
+    console.log(content);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed: File does not exist');
+    } else {
+      throw error;
+    }
+  } 
 };
 
 await read();
