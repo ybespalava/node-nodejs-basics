c40ce34976f893408517b7340b52c5167c8d375e
ybespalava <ybespalava@icloud.com>
2024-10-07 22:54:12 +0200

    feat: add code block

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = 'files/fileToRead.txt';
+  const filePath = join(__dirname, fileToRead);
+
+  const readStream = createReadStream(filePath);
+
+  readStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readStream.on('end', () => {
+    console.log('\nRead operation completed');
+  });
+
+  readStream.on('error', (error) => {
+    console.error(`FS operation failed: ${error.message}`);
+  });
 };
 
 await read();
