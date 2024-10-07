9efae66a7277cb9d68fc976c854e57e70e2bb000
ybespalava <ybespalava@icloud.com>
2024-10-07 22:34:02 +0200

    feat: add code block

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import { promises as fsPromises, constants } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const fileToRemove = 'files/fileToRemove.txt';
+  const filePath = join(__dirname, fileToRemove);
+
+  try {
+    await fsPromises.access(filePath, constants.R_OK);
+
+    await fsPromises.unlink(filePath);
+    console.log(`File ${fileToRemove} successfully removed`);
+  } catch (error) {
+
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed: File does not exist');
+    } else {
+
+      throw error;
+    }
+  }
 };
 
 await remove();
