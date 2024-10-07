e83a00e02990d7f7416bd4f75e5a5306d834b7e0
ybespalava <ybespalava@icloud.com>
2024-10-07 20:56:12 +0200

    feat: add code block

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+        const rssVariables = Object.keys(process.env).filter(key => key.startsWith('RSS_'));
+
+    rssVariables.forEach(key => {
+      const value = process.env[key];
+      console.log(`RSS_${key}=${value};`);
+    }); 
 };
 
 parseEnv();
